aa628dded8f2f435d2526b5acc078d2f63524ec4
Adam Bohannon <abo46n2@gmail.com>
2017-11-07 14:52:51 -0800

    Added scale for color

--- script.js
+++ script.js
@@ -15,11 +15,20 @@ const app = (data) => {
   const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   console.log(month)
 
+  const colors = ['#225ea8', '#41b6c4', '#a1dab4', '#ffffb2', '#fecc5c', '#fd8d3c', '#e31a1c']
+
+  const colorScale = d3.scaleQuantize()
+    .domain([
+      d3.min(tempData, d => d.variance),
+      d3.max(tempData, d => d.variance)
+    ])
+    .range(colors)
+
   // create dimensions
   const w = 1100
   const h = 600
   const padding = 100
-  const rectWidth = w / tempData.length
+  const rectWidth = tempData.length / w
   const rectHeight = (h - padding * 2) / month.length
   console.log('gridWidth', rectWidth)
 
@@ -80,9 +89,10 @@ const app = (data) => {
     .attr('y', (d, i) => (d.month - 1) * rectHeight + padding)
     .attr('year', d => (d.year)) // TODO: Remove
     .attr('month', d => (d.month)) // TODO: Remove
+    .attr('variance', d => (d.variance)) // TODO: Remove
     .attr('width', rectWidth)
     .attr('height', rectHeight)
-    .attr('fill', 'blue')
+    .attr('fill', d => colorScale(d.variance))
 }
 
 // fetch data
